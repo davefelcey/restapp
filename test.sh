754c40efb6473b7fdff7d1001ad4b111c67fda5e
david.felcey <david.felcey@oracle.com>
2018-01-10 00:25:28 +0000

    Added NLTK dependencies

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-URL='https://www.indeed.co.uk/viewjob?jk=b39ccb985a9de5e1&from=tp-serp&tk=1c29ob0im14he6rl'
-APP=http://127.0.0.1:8000
+# URL='https://www.indeed.co.uk/viewjob?jk=b39ccb985a9de5e1&from=tp-serp&tk=1c29ob0im14he6rl'
+APP=https://qrestapp.herokuapp.com/
+# APP=http://127.0.0.1:8000
 
-curl -v -G -v "$APP" --data-urlencode "url=$URL"
+curl -v -G -v "$APP" --data-urlencode "url=$1"
